5dfa34afe040e3550290f411f2cca65a3da830f5
AICI-Global-84 <aiciglobal01@gmail.com>
2024-10-16 11:49:27 +0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from .SubtitleNode import SubtitleNode
+NODE_CLASS_MAPPINGS = {
+    "SubtitleNode": SubtitleNode
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "SubtitleNode": "SubtitleNode"
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
